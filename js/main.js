3d2892916f87b95edb7a441b5f84eb42529976ea
Александр Суворов <87974742+suvorovweb@users.noreply.github.com>
2021-08-20 17:35:08 +0300

    Добавил модальное окно + выполнение доп.задания

--- js/main.js
+++ js/main.js
@@ -1,14 +1,47 @@
 $(document).ready(function () { /* функция готовности DOM */
     var currentFloor = 2; /* переменная, где хранится текущий этаж */
-    var floorPath = $('.home-image path') /* каждый отдельный этаж в SVG */
-    var counterUp = $('.counter-up')    /* кнопка увелечения этажа */
-    var counterDown = $('.counter-down') /* кнопка уменьшения этажа */
+    var floorPath = $('.home-image path'); /* каждый отдельный этаж в SVG */
+    var counterUp = $('.counter-up');    /* кнопка увелечения этажа */
+    var counterDown = $('.counter-down'); /* кнопка уменьшения этажа */
+    var modal = $('.modal');   // модальное окно //
+    var modalCloseButton = $('.modal-close-button'); // кнопка закрытия модального окна //
+    var buttonPrimary = $('.button-primary'); // кнопка //
+    var flatLinks = $('.flat-link')
+    var flatPath = $('.flats path')
+
     /* при навведении мышки на этаж */
     floorPath.on('mouseover', function(){
         floorPath.removeClass("current-floor"); /* удаляем активный класс у этажей */
         currentFloor = $(this).attr("data-floor"); /* получаем значение текущего этажа*/
         $('.counter').text(currentFloor); /* записываем значение этажа в счетчик */
     });
+
+    flatPath.on('mouseover', function(){
+        flatPath.removeClass('current-flat');
+        flatLinks.removeClass('current-flat');
+        currentFlat = $(this).attr("data-flat");
+        $(`[data-flat="${currentFlat}"]`).addClass('current-flat');
+    });
+
+    flatLinks.on('mouseover', function(){
+        flatPath.removeClass('current-flat');
+        flatLinks.removeClass('current-flat');
+        currentFlat = $(this).attr("data-flat");
+        $(`[data-flat="${currentFlat}"]`).addClass('current-flat');
+    });
+
+    floorPath.on('click', function (){
+        modal.toggleClass('is-open'); // при клике на любой элемент этажа, открывается модальное окно //
+    });
+
+    modalCloseButton.on('click', function (){
+        modal.removeClass('is-open'); // при клике на "закрывающий значок" модального окна, закроется все модальное окно
+    })
+
+
+    buttonPrimary.on('click', function (){
+        modal.toggleClass('is-open'); // при клике на кнопку, откроется модальное окно //
+    })
 
 
     counterUp.on('click', function (){
@@ -35,3 +68,4 @@ $(document).ready(function () { /* функция готовности DOM */
         }
     });
 });
+
